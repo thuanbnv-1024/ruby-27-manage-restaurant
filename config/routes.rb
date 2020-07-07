16c62a2b0158875c2d725f4928765a3c81388597
thuanbnv-1024 <nguyen.van.thuanb@sun-asterisk.com>
2020-07-07 14:42:58 +0700

    show customeradmin

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace :admin do
       root "admin#index"
       get "/login", to: "session#new"
+      resources :admin_customers
       resources :departments
       resources :dish_types
     end
