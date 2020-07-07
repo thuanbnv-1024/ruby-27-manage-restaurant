6ffa91bccf9f1b96fb2f2e2c2d760e8252f4329e
Vu Duy Tu <duytu131995@gmail.com>
2020-07-07 11:18:46 +0700

    Admin create department

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,3 +1,10 @@
 class Department < ApplicationRecord
+  DEPARTMENT_PARAMS = %i(name description).freeze
+
   has_many :users, dependent: :destroy
+
+  validates :name, presence: true, uniqueness: true,
+            length: {maximum: Settings.department.validates.name.max}
+  validates :description, presence: true,
+            length: {maximum: Settings.department.validates.description.max}
 end
